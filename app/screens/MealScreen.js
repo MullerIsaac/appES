4b43b06f66e84ea3fea14839bb8619c379fd872b
Pedro Paulo <pedropereira10p@gmail.com>
2022-07-19 13:13:16 -0300

    adicionando aas

--- app/screens/MealScreen.js
+++ app/screens/MealScreen.js
@@ -1,14 +1,33 @@
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, TextInput } from 'react-native';
 import FormButton from '../components/FormButton';
+import { windowHeight } from '../utils/Dimensions';
 import { useState } from 'react';
 
 
 export default function MealScreen(){
+    const [item, setItem] = useState();
+    const [grams, setGrams] = useState();
     const [meals, setMeals] = useState([]);
+
+    function addItem(){ 
+        setMeals();
+
+    }
 
     return(
         <View style={ styles.container }>
             <Text>Tela de cadastro de refeições</Text>
+            <View>
+                <Text>{item}</Text>
+                <Text>{grams}</Text>
+                
+            </View>
+            <View style={ styles.inputContainer }>
+                <TextInput style={ styles.input } onChangeText={item => setItem(item)} />
+                <TextInput style={ styles.input } onChangeText={grams => setGrams(grams)} />
+            </View>
+            <TouchableOpacity onPress={() => addItem()}><Text>Pei</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => setMeals([])}><Text>Reset</Text></TouchableOpacity>
         </View>
     );
 }
@@ -19,5 +38,17 @@ const styles = StyleSheet.create({
       flex: 1,
       justifyContent: "center",
       alignItems: "center",
+    },
+
+    inputContainer: {
+        flexDirection: "row",
+    },
+
+    input: {
+       width: 80,
+       height: 40,
+       backgroundColor: "#fff",
+       margin: 10
     }
+
   });
